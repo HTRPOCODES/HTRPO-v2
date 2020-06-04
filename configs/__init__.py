d4ebf6b9fe3f3f5e54992d481639fd21bb5a6729
HTRPOCODES <55749274+HTRPOCODES@users.noreply.github.com>
2020-06-04 16:57:46 +0800

    Add files via upload

--- configs/__init__.py
+++ configs/__init__.py
@@ -1,1 +1,10 @@
+import os
 
+flist = []
+files = os.listdir("configs")
+for f in files:
+    if os.path.splitext(f)[1] == '.py':
+        file = f.split('.')[0]
+        flist.append("configs." + file)
+for f in flist:
+    __import__(f)
